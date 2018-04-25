ccadb2d926cc83c509b9eb0150ade0898f6e030b
Gutenberg Neto <gutenbergn@gmail.com>
2018-04-25 17:30:39 -0300

    adding more access modifiers

--- FuzeUtils/Classes/DisplayHelper.swift
+++ FuzeUtils/Classes/DisplayHelper.swift
@@ -22,19 +22,19 @@ public enum DisplayType {
 }
 
 public final class Display {
-    class var width: CGFloat { return UIScreen.main.bounds.size.width }
-    class var height: CGFloat { return UIScreen.main.bounds.size.height }
-    class var maxLength: CGFloat { return max(width, height) }
-    class var minLength: CGFloat { return min(width, height) }
-    class var zoomed: Bool { return UIScreen.main.nativeScale >= UIScreen.main.scale }
-    class var retina: Bool { return UIScreen.main.scale >= 2.0 }
-    class var phone: Bool { return UIDevice.current.userInterfaceIdiom == .phone }
-    class var pad: Bool { return UIDevice.current.userInterfaceIdiom == .pad }
-    class var padPro1366: Bool { return UIDevice.current.userInterfaceIdiom == .pad &&
+    public class var width: CGFloat { return UIScreen.main.bounds.size.width }
+    public class var height: CGFloat { return UIScreen.main.bounds.size.height }
+    public class var maxLength: CGFloat { return max(width, height) }
+    public class var minLength: CGFloat { return min(width, height) }
+    public class var zoomed: Bool { return UIScreen.main.nativeScale >= UIScreen.main.scale }
+    public class var retina: Bool { return UIScreen.main.scale >= 2.0 }
+    public class var phone: Bool { return UIDevice.current.userInterfaceIdiom == .phone }
+    public class var pad: Bool { return UIDevice.current.userInterfaceIdiom == .pad }
+    public class var padPro1366: Bool { return UIDevice.current.userInterfaceIdiom == .pad &&
         max(Display.height, Display.width) == 1366 }
-    class var carplay: Bool { return UIDevice.current.userInterfaceIdiom == .carPlay }
-    class var tv: Bool { return UIDevice.current.userInterfaceIdiom == .tv }
-    class var typeIsLike: DisplayType {
+    public class var carplay: Bool { return UIDevice.current.userInterfaceIdiom == .carPlay }
+    public class var tv: Bool { return UIDevice.current.userInterfaceIdiom == .tv }
+    public class var typeIsLike: DisplayType {
         if phone && maxLength < 568 {
             return .iphone4
         } else if phone && maxLength == 568 {
