81294b3df8343124c30741ab955a287baaceeb41
Gutenberg Neto <gutenbergn@gmail.com>
2019-03-07 14:20:06 -0300

    adding new method to UILabel+Fade

--- FuzeUtils/Classes/UILabel+Fade.swift
+++ FuzeUtils/Classes/UILabel+Fade.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 public extension UILabel {
+    public func setAttributedTextWithFade(attributedText: NSAttributedString, duration: TimeInterval = 0.25) {
+        UIView.transition(with: self, duration: duration, options: .transitionCrossDissolve, animations: {
+            self.attributedText = attributedText
+        }, completion: nil)
+    }
+    
     public func setTextWithFade(text: String, duration: TimeInterval = 0.25) {
         UIView.transition(with: self, duration: duration, options: .transitionCrossDissolve, animations: {
             self.text = text
